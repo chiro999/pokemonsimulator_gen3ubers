55c3f57feaf32204e9563b4ee84b75b57833398b
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2024-09-19 14:50:13 +0000

    Update statinteractions.py

--- logic/statinteractions.py
+++ logic/statinteractions.py
@@ -26,10 +26,6 @@ def attack_stat(pokemon:Pokemon, weather:str, crit:bool=False) -> float:
         modifier *= 2
     if pokemon.ability == 'hustle':
         modifier *= 1.5
-    if pokemon.ability == 'guts' and pokemon.status != '':
-        modifier *= 1.5
-    if pokemon.ability == 'slowstart' and pokemon.active_turns < 5:
-        modifier = 0.5
     if pokemon.item == 'choiceband':
         modifier = 1.5
     if pokemon.species == 'pikachu' and pokemon.item == 'lightball':
